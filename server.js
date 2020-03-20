918b78f6239b23c4254d88d5dd909cbbcc8c1620
TxTony <t.tipveau@berd.re>
2020-03-20 17:59:37 +0400

    Update server.js

--- server.js
+++ server.js
@@ -16,8 +16,8 @@ try{
         
         // Set CORS headers: allow all origins, methods, and headers: you may want to lock this down in a production environment
         res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
-        res.header("Access-Control-Allow-Headers", req.header('access-control-request-headers') || 'origin, content-type, accept, location, code');
+        res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE, HEAD");
+        res.header("Access-Control-Allow-Headers", req.header('access-control-request-headers') || 'X-AUTH-TOKEN,origin, content-type, accept, location, code');
 
         if (req.method === 'OPTIONS') {
             // CORS Preflight
